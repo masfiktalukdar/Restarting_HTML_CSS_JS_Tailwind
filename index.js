6bf2abcd8f584cade7b7a98af084c73ab607f1a1
Masfik Talukdar <freelancermasfik@gmail.com>
2025-09-22 22:21:43 +0600

    Football betting app

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
 let tips = [];
@@ -21,3 +23,60 @@ for(item of bills){
 }
 
 console.log(tips, totals)
+
+
+*/
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+        'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+const {players:[players1, players2]} = game;
+const {odds:{team1, x: draw, team2 }} = game;
+
+const [gk1, ...fieldPlayers1] = players1;
+const [gk2, ... fieldPlayers2] = players2;
+
+const allPlayer = [...players1, ...players2];
+
+const players1Final = [...players1, "Thiago", 'Coutinho','Perisic'];
+
+console.log(team2)
